f7c760b056b5a80a968d150d53344f2696419cd6
Guest User <student@epicodus.com>
2015-09-10 16:48:40 -0700

    Add completed teacher unit test

--- src/test/java/TeacherTest.java
+++ src/test/java/TeacherTest.java
@@ -14,41 +14,68 @@ public class TeacherTest {
     assertEquals(Teacher.all().size(), 0);
   }
 
-  // Test for override objects
-  // @Test
-  // public void equals_returnsTrueIfNamesAretheSame() {
-  //   Activity firstActivity = new Activity("Yoga","union of the individual consciousness");
-  //   Activity secondActivity = new Activity("Yoga","union of the individual consciousness");
-  //   assertTrue(firstActivity.equals(secondActivity));
-  // }
-  //
-  // @Test
-  // public void save_savesObjectIntoDatabase() {
-  //   Activity myActivity = new Activity ("Yoga","union of the individual consciousness");
-  //   myActivity.save();
-  //   assertEquals(myActivity.all().get(0),myActivity);
-  // }
-  //
-  // @Test
-  // public void getId_returnsCorrectId() {
-  //   Activity myActivity = new Activity("Yoga","union of the individual consciousness");
-  //   myActivity.save();
-  //   assertEquals(Activity.all().get(0).getId(),myActivity.getId());
-  // }
-  //
-  // @Test
-  // public void find_returnsCorrectActivity() {
-  //   Activity myActivity = new Activity ("Yoga","union of the individual consciousness");
-  //   myActivity.save();
-  //   Activity savedActivity = Activity.find(myActivity.getId());
-  //   assertEquals(myActivity,savedActivity);
-  // }
-  //
-  // @Test
-  //  public void delete_deletesFromDatabase() {
-  //    Activity myActivity = new Activity("Yoga","union of the individual consciousness");
-  //    myActivity.save();
-  //    myActivity.delete();
-  //    assertEquals(Activity.all().size(), 0);
-  //  }
+  //Test for override objects
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame() {
+    Activity firstActivity = new Activity("Yoga","union of the individual consciousness");
+    Activity secondActivity = new Activity("Yoga","union of the individual consciousness");
+    assertTrue(firstActivity.equals(secondActivity));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase() {
+    Activity myActivity = new Activity ("Yoga","union of the individual consciousness");
+    myActivity.save();
+    assertEquals(myActivity.all().get(0),myActivity);
+  }
+
+  @Test
+  public void getId_returnsCorrectId() {
+    Activity myActivity = new Activity("Yoga","union of the individual consciousness");
+    myActivity.save();
+    assertEquals(Activity.all().get(0).getId(),myActivity.getId());
+  }
+
+  @Test
+  public void find_returnsCorrectActivity() {
+    Activity myActivity = new Activity ("Yoga","union of the individual consciousness");
+    myActivity.save();
+    Activity savedActivity = Activity.find(myActivity.getId());
+    assertEquals(myActivity,savedActivity);
+  }
+
+  @Test
+   public void delete_deletesFromDatabase() {
+     Activity myActivity = new Activity("Yoga","union of the individual consciousness");
+     myActivity.save();
+     myActivity.delete();
+     assertEquals(Activity.all().size(), 0);
+   }
+
+
+   @Test
+    public void getAllStudents() {
+      Student myStudent = new Student("chitra", 23, "503-789-3298", "xsa@gmail.com");
+      myStudent.save();
+
+
+      Activity myFirstActivity = new Activity("swimming", "aaaaaaaaa");
+      myFirstActivity.save();
+
+      Activity mySecondActivity = new Activity("yoga", "yogaaaaaaaaaa");
+      mySecondActivity.save();
+
+      Teacher myFirstTeacher = new Teacher("teach1", "Quali1", "low", 10, 23.00,"loca1", 5, "2015-09-09", "2015-10-10", "12.00");
+      myFirstTeacher.save();
+
+      Teacher mySecondTeacher = new Teacher("teach2", "Quali2", "low", 10, 23.00,"loca2", 5, "2015-09-09", "2015-10-10", "12.00");
+      mySecondTeacher.save();
+
+      myStudent.addActivity(myFirstActivity,myFirstTeacher);
+      myStudent.addActivity(mySecondActivity,mySecondTeacher);
+      assertEquals("chitra", myFirstTeacher.getStudents().get(0).getName());
+    }
+
+
+
 }
