ef5abd5fb635bf6364994bb4312cd6705b3f006f
chitra atmakuru <chitra.atmakuru@gmail.com>
2015-09-12 18:47:17 -0700

    delete extra velocityTemplateEngine.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,8 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/one_stop_activities", "chitra", "1234");
+  //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/one_stop_activities", "chitra", "1234");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/one_stop_activities", null, null);
 
 
   // Make sure to customize the url to have the actual database name.
